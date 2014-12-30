53144b2213a6e14fd9affd4c36b816be95f17f92
mclawges22 <mclawges22@gmail.com>
2014-12-30 13:52:07 -0500

    creating files and first colorwheel iteration

---
+++ js/colorwheel.js
@@ -0,0 +1,41 @@
+function pinkGirl() {
+	$("#girl").css("background-color", "#f7b5ce");
+	$("#hair").attr("fill", "#f745a8");
+}
+
+function redGirl() {
+	$("#girl").css("background-color", "#ff7a65");
+	$("#hair").attr("fill", "#b73023");
+}
+function orangeGirl() {
+	$("#girl").css("background-color", "#ffb35c");
+	$("#hair").attr("fill", "#ff8700");
+}
+function yellowGirl() {
+	$("#girl").css("background-color", "#FFE06C");
+	$("#hair").attr("fill", "#FCD100");
+}
+function greenGirl() {
+	$("#girl").css("background-color", "#00B50D");
+	$("#hair").attr("fill", "#086108");
+}
+function turquoiseGirl() {
+	$("#girl").css("background-color", "#08B5EE");
+	$("#hair").attr("fill", "#00DCB4");
+}
+function blueGirl() {
+	$("#girl").css("background-color", "#3F00BE");
+	$("#hair").attr("fill", "#0071FF");
+}
+function purpleGirl() {
+	$("#girl").css("background-color", "#462959");
+	$("#hair").attr("fill", "#a27994");
+}
+function lightsOn() {
+	$("#girl").css("background-color", "#fff");
+	$("#hair").attr("fill", "#000");
+}
+function lightsOff() {
+	$("#girl").css("background-color", "#000");
+	$("#hair").attr("fill", "#fff");
+}
