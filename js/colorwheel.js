500c0c50dfe79f86770f54db025edc1a6e005b12
mclawges22 <mclawges22@gmail.com>
2015-01-20 20:48:43 -0500

    fading background colors plus some schedule HTML

--- js/colorwheel.js
+++ js/colorwheel.js
@@ -5,6 +5,8 @@ $( document ).ready(function() {
     $("#down-triangle").css("top", windowHeight+20);
     $("#about-section").css("height", windowHeight);
     $("#about-section").css("top", windowHeight+20);
+    $("#schedule-section").css("height", windowHeight);
+    $("#schedule-section").css("top", 2*windowHeight);
 
     $(window).scroll(function () {
 	    if ($(this).scrollTop() > windowHeight+20) {
@@ -24,13 +26,26 @@ $( document ).ready(function() {
 	        $(".tagline").show();
 	        $(".down-carrot").show();
 	    }
-
 	});
+
+	$("#register").click(function () {
+		$('html, body').stop().animate({
+	        scrollTop: $( $(this).attr('href') ).offset().top + 32
+	    }, 400);
+	});
+	$("#schedule").click(function () {
+		$('html, body').stop().animate({
+	        scrollTop: $( $(this).attr('href') ).offset().top + 32
+	    }, 400);
+	});
+
 
 });
 
 function pinkGirl() {
-	$("#girl").css("background-color", "#f492a7");
+	$("#girl").animate({
+	    "background-color": "#f492a7"
+	  }, 1000 );
 	$(".hair").attr("fill", "#f44780");
 	$("#shape").attr("fill", "#f44780");
 	$("em").css("color", "#f44780");
@@ -38,49 +53,69 @@ function pinkGirl() {
 }
 
 function redGirl() {
-	$("#girl").css("background-color", "#ff7a65");
+	$("#girl").animate({
+	    "background-color": "#ff7a65"
+	  }, 1000 );
 	$(".hair").attr("fill", "#b73023");
 	$("#shape").attr("fill", "#b73023");
 	$("em").css("color", "#ff7a65");
 	$("strong").css("color", "#b73023");
+	var hair = $(".hair");
+	/*animate(0, 1500, function (val) {
+	    hair.attr({
+	        fill: "#ffffff"
+	    });
+	}, 3000);*/
 }
 function orangeGirl() {
-	$("#girl").css("background-color", "#ffb35c");
+	$("#girl").animate({
+	    "background-color": "#ffb35c"
+	  }, 1000 );
 	$(".hair").attr("fill", "#ff8700");
 	$("#shape").attr("fill", "#ff8700");
 	$("em").css("color", "#ff8700");
 	$("strong").css("color", "#ffb35c");
 }
 function yellowGirl() {
-	$("#girl").css("background-color", "#FFE06C");
+	$("#girl").animate({
+	    "background-color": "#FFE06C"
+	  }, 1000 );
 	$(".hair").attr("fill", "#FCD100");
 	$("#shape").attr("fill", "#FCD100");
 	$("em").css("color", "#FCD100");
 	$("strong").css("color", "#FFE06C");
 }
 function greenGirl() {
-	$("#girl").css("background-color", "#00B50D");
+	$("#girl").animate({
+	    "background-color": "#00B50D"
+	  }, 1000 );
 	$(".hair").attr("fill", "#086108");
 	$("#shape").attr("fill", "#086108");
 	$("em").css("color", "#086108");
 	$("strong").css("color", "#00B50D");
 }
 function turquoiseGirl() {
-	$("#girl").css("background-color", "#08B5EE");
+	$("#girl").animate({
+	    "background-color": "#08B5EE"
+	  }, 1000 );
 	$(".hair").attr("fill", "#00DCB4");
 	$("#shape").attr("fill", "#00DCB4");
 	$("em").css("color", "#08B5EE");
 	$("strong").css("color", "#00DCB4");
 }
 function blueGirl() {
-	$("#girl").css("background-color", "#0b31aa");
+	$("#girl").animate({
+	    "background-color": "#0b31aa"
+	  }, 1000 );
 	$(".hair").attr("fill", "#3291F9");
 	$("#shape").attr("fill", "#3291F9");
 	$("em").css("color", "#0b31aa");
 	$("strong").css("color", "#3291F9");
 }
 function purpleGirl() {
-	$("#girl").css("background-color", "#462959");
+	$("#girl").animate({
+	    "background-color": "#462959"
+	  }, 1000 );
 	$(".hair").attr("fill", "#a27994");
 	$("#shape").attr("fill", "#a27994");
 	$("em").css("color", "#462959");
@@ -96,3 +131,14 @@ function lightsOff() {
 	$(".hair").attr("fill", "#fff");
 	$("#shape").attr("fill", "#fff");
 }
+/*
+animate = function (from, to, setter, ms, easing, callback) {
+    if (typeof easing == "function" && !easing.length) {
+        callback = easing;
+        easing = mina.linear;
+    }
+    var now = mina.time(),
+        anim = mina(from, to, now, now + ms, mina.time, setter, easing);
+    callback && eve.once("mina.finish." + anim.id, callback);
+    return anim;
+};*/
